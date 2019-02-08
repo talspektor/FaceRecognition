da303ba1a2191835cf91569f14fbeb7e6db5ac26
user140592 <user140592@eu201.local>
2019-02-08 12:31:09 +0000

    Initial Commit

---
+++ FaceRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceRecognition
+//
+//  Created by user140592 on 2/8/19.
+//  Copyright © 2019 talspektor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
